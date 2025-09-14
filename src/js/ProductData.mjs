e539cea71e74a237cf538b437f133a0865404e48
papy-pro <jiresselusa127@gmail.com>
2025-09-14 12:50:43 -0700

    ad

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,26 +1,22 @@
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
-}
-
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    // Chemin dynamique, compatible GitHub Pages
-    this.path = `${import.meta.env.BASE_URL}json/${this.category}.json`;
+
+    // Compatible local et GitHub Pages
+    const base = (import.meta.env && import.meta.env.BASE_URL) ? import.meta.env.BASE_URL : "./";
+    this.path = `${base}json/${this.category}.json`;
   }
 
   getData() {
     return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+      .then(res => {
+        if (!res.ok) throw new Error("Bad Response");
+        return res.json();
+      });
   }
 
   async findProductById(id) {
     const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    return products.find(item => item.Id === id);
   }
 }
